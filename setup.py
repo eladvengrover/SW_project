746ceab15e9e2e5996d3adce6b1875b6cb40e886
Elad <eladv@mail.tau.ac.il>
2023-08-20 11:38:00 +0300

    changed setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import Extension, setup
 
-module = Extension("fit_capi", sources=['kmeansmodule.c'])
-setup(name='fit_capi',
+module = Extension("symnmfAPI", sources=['symnmfmodule.c'])
+setup(name='symnmfAPI',
      version='1.0',
      description='Python wrapper for custom C extension',
      ext_modules=[module])
-
-# TODO - change it according to this project
--- symnmf.py
+++ symnmf.py
@@ -1,9 +1,25 @@
 import sys
+import pandas as pd
+# import symnmfAPI
+
+
+SYMNMF = "symnmf"
+SYM = "sym"
+DDG = "ddg"
+NORM = "norm"
+
+
 
 args = sys.argv
 
 K = args[1]
 goal = args[2]
 file_name = args[3]
+
+X = pd.read_csv(file_name, header=None)
+
+if goal == SYM:
+    # symnmfAPI.symnmf()
+    pass
 
 
